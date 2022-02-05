0bfc5ad526c234f257d67955cd835d35fa280951
Yuta <y.sugiyama54@gmail.com>
2022-02-05 23:40:07 +0900

    論理値

---
+++ src/lesson3_7.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	t, f := true, false
+	fmt.Printf("%T %v\n", t, t)
+	fmt.Printf("%T %v\n", f, f)
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(false && false)
+
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(false || false)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
