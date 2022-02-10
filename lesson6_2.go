a365607fe22be2bcedb27ac1f96b223f9f10dc9d
Yuta <y.sugiyama54@gmail.com>
2022-02-11 00:17:48 +0900

    コンストラクタ

---
+++ lesson6_2.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	x, y int
+}
+
+func (v Vertex) Area() int {
+	return v.x * v.y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+func New(x, y int) *Vertex {
+	return &Vertex{x, y}
+}
+
+func main() {
+	v := New(3, 4)
+	v.Scale(10)
+	fmt.Println(v)
+	fmt.Println(v.Area())
+}
